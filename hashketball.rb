752cc8e5a63c8bb3ae0dabca9913356c0af1f1b9
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-30 22:51:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,7 @@ end
 def num_points_scored(players_name)
   points = game_hash[:home][:players][:points]
     if players_name == game_hash[:home][:players][:players_name]
-      points
+      points.to_i
     end 
 end 
 
