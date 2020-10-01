103fc228183a1194bd5215f4260233d2fd2e43e8
Connor Mulholland <mulhollandconnor@icloud.com>
2020-10-01 01:59:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,9 @@ end
 # Write code here
 def num_points_scored(players_name)
   game_hash.each do |team, team_info|
-    team_info[:players].each do |
+    team_info[:players].each do |player|
+      if player[:player_name] == players_name
+        player[:points]
     
 def shoe_size(players_name)
   
