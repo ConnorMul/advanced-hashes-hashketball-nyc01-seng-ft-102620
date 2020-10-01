34a719c9a7637306e5c13c27ff8de91f1e484032
Connor Mulholland <mulhollandconnor@icloud.com>
2020-10-01 00:43:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,8 +166,7 @@ end
 
 def player_stats(players_name)
   stats = []
-if players_name == game_hash[:home][:players][:player_name] || players_name[:home][:players][:player_name]
-  stats << game_hash[:home][:players]
+
   
   end
     stats.flatten
