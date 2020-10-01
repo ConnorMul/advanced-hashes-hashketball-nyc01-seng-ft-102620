5f0208b52782b6dc76288440db025a8162e829d2
Connor Mulholland <mulhollandconnor@icloud.com>
2020-10-01 03:24:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -183,8 +183,9 @@ def player_stats(players_name)
  stats = {}
  
    game_hash.each do |team, team_info|
-    players_name = team_info[:players]
-    stats = players_name
+    team_info[:players].each do |player_info|
+      if players_name == player_info[:player_name]
+      stats = player_info
    end 
    stats
 end
