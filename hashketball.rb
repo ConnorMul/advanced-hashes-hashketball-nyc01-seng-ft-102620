a5bd2656cc11be00f277214d0116aae729e43627
Connor Mulholland <mulhollandconnor@icloud.com>
2020-10-01 01:23:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -153,7 +153,7 @@ end
 def team_names
     game_hash.each do |key, value|
       value.each do |key2, value2|
-        value2.fetch_value(:team_name)
+        value2.fetch_values(:team_name)
       end
     end
  end 
